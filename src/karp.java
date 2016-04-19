83dbae1c2f12dd00deaf988a7b04e62add1b7203
andrewpaek <andrew.k.paek@gmail.com>
2016-04-19 00:23:48 -0400

    kk algo all working

---
+++ src/karp.java
@@ -0,0 +1,46 @@
+/**
+ * Created by Andrew on 4/17/2016.
+ */
+public class karp {
+
+
+//    Outline:
+//    1. turn input list into a max heap - create new empty array, turn into binary heap
+//    2. extract two largest values in constant time
+//    3. subtract values
+//    4. insert difference of subtraction
+
+    public int test1(int[] input_list){
+
+        // initializing the max heap with our input list - one time nlogn time
+
+        int marker = (input_list.length+1)%2;
+
+        MaxHeap m_heap = new MaxHeap(input_list.length+1);
+        for (int i:input_list){
+            m_heap.insert(i);
+        }
+
+
+        int m1;
+        int m2;
+        int difference;
+        while(m_heap.size>1){
+            m1=m_heap.remove();
+            m2=m_heap.remove();
+            difference=m1-m2;
+            m_heap.insert(difference);
+        }
+        return m_heap.remove();
+
+    }
+
+
+    public void print_list(int[] input_list){
+        for (int i:input_list){
+            System.out.println(i);
+        }
+    }
+
+
+}
---
+++ src/karp_main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Andrew on 4/17/2016.
+ */
+public class karp_main{
+    public static void main(String[] args){
+        // This test case breaks when the if I take out the if caluse that I added to maxheap. Think about that...
+        int[] testing = {17, 13, 10, 4, 2, 1};
+        karp k = new karp();
+        int m = k.test1(testing);
+        System.out.println("Residue: "+m);
+    }
+}
